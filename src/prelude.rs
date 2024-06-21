69f6f940f6aa53549e9b88ac52ed0ab55a923006
sergerad <serge.radinovich@immutable.com>
2024-06-21 19:25:56 +1200

    workflow

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,7 +3,3 @@ pub use crate::error::Error;
 
 // Result type using crate's error type
 pub type Result<T> = core::result::Result<T, Error>;
-
-// Generic wrapper tuple struct for newtype pattern.
-// Allows us to impl external trait on external type.
-pub struct NewType<T>(pub T);
